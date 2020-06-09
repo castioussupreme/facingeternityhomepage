482a0aac06c2c44f13e39fc0bf96f3c9cf723b06
YoWenqin <wchen15@smith.edu>
2020-06-08 17:46:51 -0700

    add social icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MDBContainer, MDBFooter } from "mdbreact";
 
+const { SocialIcon } = require('react-social-icons');
 const Footer = () => {
   return (
     <MDBFooter
@@ -9,6 +10,11 @@ const Footer = () => {
     >
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
+        <SocialIcon network="facebook" url="https://www.facebook.com/facingeternity/" style={{ height: 50, width: 50 }} fgColor="#FFFFFF" bgColor="transparent" />
+        <SocialIcon network="instagram" url="https://www.instagram.com/facingeternity/" style={{ height: 50, width: 50 }} fgColor="#FFFFFF" bgColor="transparent"/>
+        <SocialIcon network="twitter" url="https://twitter.com/FacingEternity" style={{ height: 50, width: 50}} fgColor="#FFFFFF" bgColor="transparent"/>
+        <SocialIcon network="youtube" url="https://www.youtube.com/channel/UC22woGikWBN9uP4xMlvgjbQ" style={{ height: 50, width: 50}} fgColor="#FFFFFF" bgColor="transparent"/>
+          <br></br>         
           &copy; {new Date().getFullYear()} Copyright:{" "}
           <a href="https://facingeternity.org"> facingeternity.org </a>
         </MDBContainer>
